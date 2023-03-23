bdf86704ebc7e9fbd9dc44f30987a1701b226186
northei <ole.thomas.theisen@jmhansen.no>
2023-03-23 09:37:09 +0100

    vd

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,10 +1,10 @@
-const { description } = require('../../package')
+const { description } = require("../../package");
 
 module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: "Vuepress Docs Boilerplate",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -16,9 +16,12 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
   ],
 
   /**
@@ -27,55 +30,52 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    logo: '/images/JM Hansen_Logo.png',
-    repo: 'olethomas93/vuepressCMS',
+    logo: "/images/JM Hansen_Logo.png",
+    repo: "olethomas93/vuepressCMS",
     editLinks: false,
-    docsDir: 'src',
-    editLinkText: '',
+    docsDir: "src",
+    editLinkText: "",
     lastUpdated: false,
     nav: [
       {
-        text: 'kunder',
-        ariaLabel: 'kunder',
-        link:'/kunder/',
+        text: "kunder",
+        ariaLabel: "kunder",
+        link: "/docs/kunder/",
         items: [
-            {text: 'Arnøy laks', link: '/kunder/arnoylaks/'},
-            {text: 'Flakstadvåg laks', link: '/kunder/flakstadvag/'},
-            {text: 'Lerøy', link: '/kunder/leroy/'},
-            {text: 'NRS', link: '/kunder/nrs/'},
-            {text: 'Salaks', link: '/kunder/salaks/'},
-            {text: 'Salmar', link: '/kunder/salmar/'}
-        ]
-    },
-    {
-      text: 'Admin',
-      link: '/admin/#/',
-    },
-      {
-        text: 'Utstyr',
-        link: '/manual/',
-        items:[
-          {text:"Montering SLS",link:'/manual/montSLS' },
-          {text:"Miljøsensor",link:'/manual/vedMiljoSen' }
-        ] 
+          { text: "Arnøy laks", link: "/kunder/arnoylaks/" },
+          { text: "Flakstadvåg laks", link: "/kunder/flakstadvag/" },
+          { text: "Lerøy", link: "/kunder/leroy/" },
+          { text: "NRS", link: "/kunder/nrs/" },
+          { text: "Salaks", link: "/kunder/salaks/" },
+          { text: "Salmar", link: "/kunder/salmar/" },
+        ],
       },
       {
-        text: 'Utstyr',
-        ariaLabel: 'Hardware',
+        text: "Admin",
+        link: "/docs/admin/#/",
+      },
+      {
+        text: "Utstyr",
+        link: "/docs/manual/",
         items: [
-            {text: 'SLS', link: '/utstyr/SLS/'},
-            {text: 'Oksygen sensorer', link: '/utstyr/oksygensensorer/'}
-        ]
-    },
+          { text: "Montering SLS", link: "/manual/montSLS" },
+          { text: "Miljøsensor", link: "/manual/vedMiljoSen" },
+        ],
+      },
+      {
+        text: "Utstyr",
+        ariaLabel: "Hardware",
+        items: [
+          { text: "SLS", link: "/docs/utstyr/SLS/" },
+          { text: "Oksygen sensorer", link: "/utstyr/oksygensensorer/" },
+        ],
+      },
     ],
-    sidebar: 'auto'
+    sidebar: "auto",
   },
 
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
-}
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+};
